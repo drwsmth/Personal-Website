4d9e548f916d3765802b99124fcd4af3591913ab
Drew Smith <drew_smith@me.com>
2017-10-09 13:41:05 +0800

    PHP Testing
    Added Spotify API GET request.

---
+++ spotify.php
@@ -0,0 +1,7 @@
+GET /v1/me/top/artists HTTP/1.1
+Host: api.spotify.com
+Accept: application/json
+Content-Type: application/json
+Accept-Encoding: gzip, deflate, compress
+Authorization: Bearer BQDF-TlQegc27qiQm5xhFCWAOjGm_zY3WSGou5SHnG_lVYUHksCkJR5SY3sZ2eYXkMG_opLcE5UAYo-h7v3aL5LupVpMxd2WmSfBb86qILhtrs1RLiHL7X1LTyTHUC-A2m9ipoNo7XH-Gn-42FVf
+User-Agent: Spotify API Console v0.1
